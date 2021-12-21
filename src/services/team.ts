ca884a44153face718de3d8bbf4eb7ff13877134
Noah Celuch <noah.celuch@gmail.com>
2021-12-21 17:19:01 -0500

    feat(user): added user request roster to team functionality (#10)
    * wip: work on create user route
    
    * test: initial setup for tests
    
    * ci: test workflow fix
    
    * ci: test workflow fix
    
    * ci: added coverage report for ci
    
    * feat: update README.md
    
    * feat(user): added user sign up service
    
    * test(user): set up user model and service tests
    
    * feat(user): added api route for create user
    
    * test: removed unnecessary test file
    
    * feat(user): added error message handling
    
    * feat(user): added luser login service
    
    * feat(user): added logout service
    
    * feat(user): added username to user model
    
    * feat(user): added login route
    
    * ci: remove package-lock for ci purposes
    
    * feat(user): added logout route
    
    * feat(user): added get own profile
    
    * feat(user): added get user service
    
    * feat(user): added get user route
    
    * feat(user): added logout all functionality
    
    * feat(user): added logout all route
    
    * feat(user): added service to delete a user
    
    * feat(user): added delete user route
    
    * feat(team): initial team model set up
    
    * feat(team): added team virtuals for populating users
    
    * feat(team): added create team service
    
    * feat(team): created create team route
    
    * feat(team): added get team service call
    
    * feat(team): added public get team route
    
    * feat(team): added get team functionality
    
    * feat(team): added tteam service roster player
    
    * feat(team): added team request roster player endpoint
    
    * fix(team): added check for already on roster on roster request
    
    * feat(user): added request to join team service
    
    * feat(user): added user request roster route

--- src/services/team.ts
+++ src/services/team.ts
@@ -13,6 +13,12 @@ export default class TeamServices {
         this.userModel = userModel
     }
 
+    /**
+     * Creates a team
+     * @param team team to create
+     * @param user user that is the manager
+     * @returns the created team
+     */
     createTeam = async (team: ITeam, user: IUserDocument): Promise<ITeamDocument> => {
         const teamObject = await this.teamModel.create(team)
 
@@ -53,6 +59,12 @@ export default class TeamServices {
         return teamObject
     }
 
+    /**
+     * gets a team that is managed by the requesting user
+     * @param teamId team id that is requested
+     * @param userId user that is the manager
+     * @returns the team document found
+     */
     getManagedTeam = async (teamId: string, userId: string): Promise<ITeamDocument> => {
         const team = await this.getTeam(teamId, false)
         for (const mId of team.managers) {
@@ -63,7 +75,14 @@ export default class TeamServices {
         throw new ApiError(Constants.UNAUTHORIZED_TO_GET_TEAM, 401)
     }
 
-    rosterPlayer = async (managerId: string, teamId: string, playerId: string): Promise<ITeamDocument> => {
+    /**
+     * Method to add a request that a player join the roster
+     * @param managerId manager of team
+     * @param teamId id of team
+     * @param userId player to request
+     * @returns updated team document
+     */
+    rosterPlayer = async (managerId: string, teamId: string, userId: string): Promise<ITeamDocument> => {
         const team = await this.teamModel.findById(teamId)
         // handle non-found team case
         if (!team) {
@@ -75,26 +94,30 @@ export default class TeamServices {
             throw new ApiError(Constants.UNAUTHORIZED_TO_GET_TEAM, 401)
         }
 
-        const user = await this.userModel.findById(playerId)
+        const user = await this.userModel.findById(userId)
         if (!user) {
             throw new ApiError(Constants.UNABLE_TO_FIND_USER, 404)
         }
 
         const teamObjectId = new Types.ObjectId(teamId)
-        const playerObjectId = new Types.ObjectId(playerId)
+        const userObjectId = new Types.ObjectId(userId)
 
         // throw error if team has already requested to roster player
-        if (user.requestsFromTeams?.includes(teamObjectId) || team.requestsToPlayers.includes(playerObjectId)) {
+        if (user.requestsFromTeams?.includes(teamObjectId) || team.requestsToPlayers.includes(userObjectId)) {
             throw new ApiError(Constants.TEAM_ALREADY_REQUESTED, 400)
         }
 
         // throw error if player has already requested to be on team's roster
-        if (user.requestsToTeams?.includes(teamObjectId) || team.requestsFromPlayers.includes(playerObjectId)) {
+        if (user.requestsToTeams?.includes(teamObjectId) || team.requestsFromPlayers.includes(userObjectId)) {
             throw new ApiError(Constants.PLAYER_ALREADY_REQUESTED, 400)
         }
 
+        if (user.playerTeams?.includes(teamObjectId) || team.players.includes(userObjectId)) {
+            throw new ApiError(Constants.PLAYER_ALREADY_ROSTERED, 400)
+        }
+
         user.requestsFromTeams?.push(teamObjectId)
-        team.requestsToPlayers.push(playerObjectId)
+        team.requestsToPlayers.push(userObjectId)
 
         await team.save()
         await user.save()
--- src/services/user.ts
+++ src/services/user.ts
@@ -1,6 +1,8 @@
-import { IUserModel } from '../models/user'
+import Team from '../models/team'
+import User, { IUserModel } from '../models/user'
 import { ApiError, IUser, IUserDocument } from '../types'
 import * as Constants from '../utils/constants'
+import { Types } from 'mongoose'
 
 export default class UserServices {
     userModel: IUserModel
@@ -9,6 +11,11 @@ export default class UserServices {
         this.userModel = userModel
     }
 
+    /**
+     * Method to sign a user up
+     * @param user data of user to sign up
+     * @returns created user document and an authentication token
+     */
     signUp = async (user: IUser): Promise<{ user: IUserDocument; token: string }> => {
         const userObject = await this.userModel.create(user)
         const token = await userObject.generateAuthToken()
@@ -16,6 +23,12 @@ export default class UserServices {
         return { user: userObject, token }
     }
 
+    /**
+     * Method to generate an authentication token for a user
+     * Actual email/password check occurs in passport.js
+     * @param email email to login with
+     * @returns the authentication token
+     */
     login = async (email: string): Promise<string> => {
         const user = await this.userModel.findOne({ email })
         const token = await user?.generateAuthToken()
@@ -27,6 +40,11 @@ export default class UserServices {
         }
     }
 
+    /**
+     * Method to logout
+     * @param email Email of user to logout
+     * @param jwt authentication token to remove from user's list
+     */
     logout = async (email: string, jwt: string) => {
         const user = await this.userModel.findOne({ email })
         if (!user) {
@@ -36,6 +54,10 @@ export default class UserServices {
         await user.save()
     }
 
+    /**
+     * Method to logout all devices
+     * @param email email of user to delete all authentication tokens
+     */
     logoutAll = async (email: string) => {
         const user = await this.userModel.findOne({ email })
         if (!user) {
@@ -45,6 +67,11 @@ export default class UserServices {
         await user.save()
     }
 
+    /**
+     * Method to get a user's public details
+     * @param id id of user to get
+     * @returns user document
+     */
     getUser = async (id: string): Promise<IUserDocument> => {
         const user = await this.userModel.findById(id)
 
@@ -63,7 +90,56 @@ export default class UserServices {
         return user
     }
 
+    /**
+     * Method to delete a user
+     * @param id id of user to delete
+     */
     deleteUser = async (id: string) => {
         await this.userModel.deleteOne({ _id: id })
+    }
+
+    /**
+     * Method to request being rostered on a team
+     * @param teamId id of team to join
+     * @param userId id of player
+     * @returns updated user document
+     */
+    requestRoster = async (teamId: string, userId: string): Promise<IUserDocument> => {
+        const team = await Team.findById(teamId)
+        const user = await User.findById(userId)
+
+        if (!team) {
+            throw new ApiError(Constants.UNABLE_TO_FIND_TEAM, 404)
+        }
+
+        if (!user) {
+            throw new ApiError(Constants.UNABLE_TO_FIND_USER, 404)
+        }
+
+        const teamObjectId = new Types.ObjectId(teamId)
+        const userObjectId = new Types.ObjectId(userId)
+
+        // throw error if player is already requested to join team
+        if (team.requestsFromPlayers?.includes(userObjectId) || user.requestsToTeams?.includes(teamObjectId)) {
+            throw new ApiError(Constants.PLAYER_ALREADY_REQUESTED, 400)
+        }
+
+        // throw error if team already requested player to join team
+        if (team.requestsToPlayers?.includes(userObjectId) || user.requestsFromTeams?.includes(teamObjectId)) {
+            throw new ApiError(Constants.TEAM_ALREADY_REQUESTED, 400)
+        }
+
+        // throw error if player already joined team
+        if (team.players.includes(userObjectId) || user.playerTeams?.includes(teamObjectId)) {
+            throw new ApiError(Constants.TEAM_ALREADY_JOINED, 400)
+        }
+
+        team.requestsFromPlayers.push(userObjectId)
+        user.requestsToTeams?.push(teamObjectId)
+
+        await team.save()
+        await user.save()
+
+        return user
     }
 }
