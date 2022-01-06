431188ce60ecf404c3c4e182f8197321a1285c4f
Noah Celuch <noah.celuch@gmail.com>
2022-01-06 14:51:32 -0500

    chore: updated dependencies to work in live environment

--- src/app.ts
+++ src/app.ts
@@ -1,16 +1,20 @@
+import cors from 'cors'
 import express, { Application } from 'express'
 import dotenv from 'dotenv'
 import { connectDatabase } from './loaders/mongoose'
-dotenv.config({ path: './config/.env' })
 import * as Routes from './routes'
 import passport from 'passport'
-require('./loaders/passport')
+
+const pathToEnv = process.cwd() + '/src/config/.env'
+dotenv.config({ path: pathToEnv })
 
 connectDatabase()
 
 const app: Application = express()
+app.use(cors())
 app.use(express.json())
 app.use(passport.initialize())
+require('./loaders/passport')
 app.use(Routes.userRouter)
 app.use(Routes.teamRouter)
 app.use(Routes.rosterRequestRouter)
