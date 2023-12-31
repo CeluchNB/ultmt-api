3f6042704c277f7253b40da658a1ecd07f43b627
Noah Celuch <noah.celuch@gmail.com>
2023-12-31 14:35:23 -0500

    ci: performance startup optimizations

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,33 @@
+console.log('Importing cors', Date.now())
 import cors from 'cors'
+console.log('Importing express', Date.now())
 import express, { Application } from 'express'
-import { router as v1Router } from './routes/v1'
+import { createLazyRouter } from 'express-lazy-router'
+console.log('Importing router', Date.now())
+// import { router as v1Router } from './routes/v1'
+console.log('Importing passport', Date.now())
 import passport from 'passport'
+console.log('Importing sendgrid', Date.now())
 import './loaders/sendgrid'
 
 const app: Application = express()
+console.log('Setting middleware', Date.now())
 app.use(cors())
 app.use(express.json())
 
 app.use(passport.initialize())
-require('./loaders/passport')
+console.log('Importing passport', Date.now())
+import './loaders/passport'
 
+console.log('Adding router', Date.now())
+const lazyRouter = createLazyRouter()
 // Version 1 of API
-app.use('/api/v1', v1Router)
+app.use(
+    '/api/v1',
+    lazyRouter(() => import('./routes/v1')),
+)
 
+console.log('Adding info endpoint', Date.now())
 app.get('/ultmt', async (req, res) => {
     res.json({ message: 'The official API of The Ultmt App' })
 })
--- src/loaders/passport.ts
+++ src/loaders/passport.ts
@@ -1,19 +1,27 @@
+console.log('PASSPORT LOADER -> Importing passport', Date.now())
 import passport from 'passport'
+console.log('PASSPORT LOADER -> Importing passport local', Date.now())
 import passportLocal from 'passport-local'
+console.log('PASSPORT LOADER -> Importing passport jwt', Date.now())
 import passportJwt, { StrategyOptions } from 'passport-jwt'
-import User from '../models/user'
+console.log('PASSPORT LOADER -> Importing bcrypt', Date.now())
 import bcrypt from 'bcryptjs'
+console.log('PASSPORT LOADER -> Importing constants', Date.now())
 import * as Constants from '../utils/constants'
+console.log('PASSPORT LOADER -> Importing redis client', Date.now())
 import { client } from './redis'
+console.log('PASSPORT LOADER -> Importing types', Date.now())
 import { ApiError } from '../types'
 
 const LocalStrategy = passportLocal.Strategy
 const JwtStrategy = passportJwt.Strategy
 
+console.log('PASSPORT LOADER -> passport use local', Date.now())
 passport.use(
     new LocalStrategy(
         { usernameField: 'email', passwordField: 'password', session: false },
         async (email, password, done) => {
+            const { default: User } = await import('../models/user')
             const user = await User.findOne({ $or: [{ email }, { username: email }] })
             if (!user) {
                 return done(null, false, { message: Constants.UNABLE_TO_LOGIN })
@@ -27,6 +35,7 @@ passport.use(
         },
     ),
 )
+console.log('PASSPORT LOADER -> Done passport use local', Date.now())
 
 const ExtractJwt = passportJwt.ExtractJwt
 const opts: StrategyOptions = {
@@ -41,8 +50,10 @@ const opts: StrategyOptions = {
     },
 }
 
+console.log('PASSPORT LOADER -> passport use jwt', Date.now())
 passport.use(
     new JwtStrategy(opts, async (jwtPayload, done) => {
         return done(null, { id: jwtPayload.sub })
     }),
 )
+console.log('PASSPORT LOADER -> Done passport use jwt', Date.now())
