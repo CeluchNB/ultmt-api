8f9d0d3ddfcbc8329c1009d5e77d578e2c7761f5
Noah Celuch <noah.celuch@gmail.com>
2024-01-01 15:40:02 -0500

    chore: remove console statements

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,16 @@
-console.log('Importing cors', Date.now())
 import cors from 'cors'
-console.log('Importing express', Date.now())
 import express, { Application } from 'express'
 import { createLazyRouter } from 'express-lazy-router'
-console.log('Importing router', Date.now())
-// import { router as v1Router } from './routes/v1'
-console.log('Importing passport', Date.now())
 import passport from 'passport'
-console.log('Importing sendgrid', Date.now())
 import './loaders/sendgrid'
 
 const app: Application = express()
-console.log('Setting middleware', Date.now())
 app.use(cors())
 app.use(express.json())
 
 app.use(passport.initialize())
-console.log('Importing passport', Date.now())
 import './loaders/passport'
 
-console.log('Adding router', Date.now())
 const lazyRouter = createLazyRouter()
 // Version 1 of API
 app.use(
@@ -27,7 +18,6 @@ app.use(
     lazyRouter(() => import('./routes/v1')),
 )
 
-console.log('Adding info endpoint', Date.now())
 app.get('/ultmt', async (req, res) => {
     res.json({ message: 'The official API of The Ultmt App' })
 })
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
-console.log('Very first thing', Date.now())
 import dotenv from 'dotenv'
 import { connectDatabase } from './loaders/mongoose'
 import { connectRedis } from './loaders/redis'
@@ -7,22 +6,15 @@ const pathToEnv = process.cwd() + '/src/config/.env'
 dotenv.config({ path: pathToEnv })
 
 const start = async () => {
-    console.log('Connecting Mongo', Date.now())
     await connectDatabase()
-    console.log('After connect Mongo', Date.now())
-    console.log('Before connect Redis', Date.now())
     await connectRedis()
-    console.log('After connect Redis', Date.now())
 
-    console.log('Before import app', Date.now())
     // eslint-disable-next-line @typescript-eslint/no-var-requires
     const app = require('./app').default
-    console.log('After import app', Date.now())
 
     const PORT = process.env.PORT || 3000
 
     app.listen(PORT, (): void => {
-        console.log('Listening', Date.now())
         return console.log(`Server started on port: ${PORT}`)
     })
 }
